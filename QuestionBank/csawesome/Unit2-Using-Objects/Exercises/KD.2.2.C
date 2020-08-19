fb456564af7e2f18826826555ad8576b4a35bf13
Brad Miller <brad@runestoneinteractive.com>
2020-08-19 20:54:13 +0000

    second pass of Qs

--- QuestionBank/csawesome/Unit2-Using-Objects/Exercises/KD.2.2.C
+++ QuestionBank/csawesome/Unit2-Using-Objects/Exercises/KD.2.2.C
@@ -1,8 +1,10 @@
 .. mchoice:: KD.2.2.C
    :author: Kuri DiFede
-   :difficulty: 1
+   :difficulty: 1.0
    :basecourse: csawesome
-   :topic: Unit2-Using-Objects/Exercises
+   :chapter: Unit2-Using-Objects
+   :subchapter: Exercises
+   :topics: Unit2-Using-Objects/Exercises
    :from_source: F
    :practice: T
    :answer_a: A constructor is something that holds the private state of an instance
@@ -14,5 +16,9 @@
    :feedback_b: 
    :feedback_c: 
    :feedback_d: 
+   :pct_on_first: 0.7039106145
+   :total_students_attempting: 179
+   :num_students_correct: 170.0
+   :mean_clicks_to_correct: 1.4647058824
 
    What is a constructor in Java?
